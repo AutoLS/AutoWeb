bf62884b66cb97a8641a3c2bc5970b39f42e0b7c
AutoLs <karston_manho@hotmail.com>
2022-04-03 12:33:09 -0700

    Implemented logger

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const logger = (req, res, next) =>
+{
+    const currentDate = new Date();
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} ${currentDate.toLocaleDateString()} ${currentDate.toLocaleTimeString()}`);
+    next();
+}
+
+app.use(logger);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/getRandomNum', (req, res) => {
