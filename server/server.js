868166872ba39391fbe40be6159e3d91a1bd6e95
AutoLs <karston_manho@hotmail.com>
2022-11-22 21:01:35 -0800

    Configure for react

--- server/server.js
+++ server/server.js
@@ -22,6 +22,12 @@ app.use(logger);
 app.use(express.json());
 const clientPath = `${__dirname}/../public`;
 app.use(express.static(clientPath));
+
+app.get('\*', (req, res) => {
+    console.log(req.url);
+    res.sendFile(req.url + '/index.html');
+});
+
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
